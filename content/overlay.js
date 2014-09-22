c4a186f9d05045f5191bd52a053c7a78aa18efbb
buggyj <buggyjef@gmail.com>
2014-09-22 22:55:58 +0200

    first try

--- content/overlay.js
+++ content/overlay.js
@@ -4,6 +4,190 @@ The JavaScript code in this file is executed via `overlay.xul` when Firefox star
 
 */
 var TiddlyFox = (function () {
+var util = {
+	
+	Date : function () {
+		this.date = new Date();
+	},
+
+	CreateAPI : function (name, index, array) {
+			util.Date.prototype[name] =function () {
+			return(this.date[name]());
+		}
+	}
+};
+["getDate",   "getFullYear",   "getMonth",   "getDay",   "getHours",   "getMinutes",   "getSeconds",   "getMilliseconds"   ].forEach(util.CreateAPI);
+["getUTCDate","getUTCFullYear","getUTCMonth","getUTCDay","getUTCHours","getUTCMinutes","getUTCSeconds","getUTCMilliseconds"].forEach(util.CreateAPI);
+["getTime","getTimezoneOffset"].forEach(util.CreateAPI);
+
+
+// ---------------------------------------------------------------------------------
+// Start of Utility functions copied from TiddlyWiki.
+// ---------------------------------------------------------------------------------
+
+
+
+
+
+var zeroPad = function(n,d)
+{
+    var s = n.toString();
+    if(s.length < d)
+            s = "000000000000000000000000000".substr(0,d-s.length) + s;
+    return(s);
+}
+
+// Convert a date to UTC YYYYMMDDHHMM string format
+util.Date.prototype.convertToYYYYMMDDHHMM = function()
+{
+    return(zeroPad(this.getUTCFullYear(),4) + zeroPad(this.getUTCMonth()+1,2) + zeroPad(this.getUTCDate(),2) + zeroPad(this.getUTCHours(),2) + zeroPad(this.getUTCMinutes(),2));
+}
+
+// Convert a date to UTC YYYYMMDD.HHMMSSMMM string format
+util.Date.prototype.convertToYYYYMMDDHHMMSSMMM = function()
+{
+	return(zeroPad(this.getUTCFullYear(),4) + zeroPad(this.getUTCMonth()+1,2) + zeroPad(this.getUTCDate(),2) + "." + zeroPad(this.getUTCHours(),2) + zeroPad(this.getUTCMinutes(),2) + zeroPad(this.getUTCSeconds(),2) + zeroPad(this.getUTCMilliseconds(),4));
+}
+
+
+
+
+// Convert a date to UTC YYYY-MM-DD HH:MM string format
+util.Date.prototype.convertToFullDate = function()
+{
+    return(zeroPad(this.getUTCFullYear(),4) + "-" +
+           zeroPad(this.getUTCMonth()+1,2) + "-" + 
+           zeroPad(this.getUTCDate(),2) + " " + 
+           zeroPad(this.getUTCHours(),2) + ":" + 
+           zeroPad(this.getUTCMinutes(),2));
+}
+
+util.Date.prototype.formatString = function(template)
+{
+	var t = template.replace(/0hh12/g,zeroPad(this.getHours12(),2));
+	t = t.replace(/hh12/g,this.getHours12());
+	t = t.replace(/0hh/g,zeroPad(this.getHours(),2));
+	t = t.replace(/hh/g,this.getHours());
+	t = t.replace(/mmm/g,dateS.shortMonths[this.getMonth()]);
+	t = t.replace(/0mm/g,zeroPad(this.getMinutes(),2));
+	t = t.replace(/mm/g,this.getMinutes());
+	t = t.replace(/0ss/g,zeroPad(this.getSeconds(),2));
+	t = t.replace(/ss/g,this.getSeconds());
+	t = t.replace(/[ap]m/g,this.getAmPm().toLowerCase());
+	t = t.replace(/[AP]M/g,this.getAmPm().toUpperCase());
+	t = t.replace(/wYYYY/g,this.getYearForWeekNo());
+	t = t.replace(/wYY/g,zeroPad(this.getYearForWeekNo()-2000,2));
+	t = t.replace(/YYYY/g,this.getFullYear());
+	t = t.replace(/YY/g,zeroPad(this.getFullYear()-2000,2));
+	t = t.replace(/MMM/g,dateS.months[this.getMonth()]);
+	t = t.replace(/0MM/g,zeroPad(this.getMonth()+1,2));
+	t = t.replace(/MM/g,this.getMonth()+1);
+	t = t.replace(/0WW/g,zeroPad(this.getWeek(),2));
+	t = t.replace(/WW/g,this.getWeek());
+	t = t.replace(/DDD/g,dateS.days[this.getDay()]);
+	t = t.replace(/ddd/g,dateS.shortDays[this.getDay()]);
+	t = t.replace(/0DD/g,zeroPad(this.getDate(),2));
+	t = t.replace(/DDth/g,this.getDate()+this.daySuffix());
+	t = t.replace(/DD/g,this.getDate());
+	var tz = this.getTimezoneOffset();
+	var atz = Math.abs(tz);
+	t = t.replace(/TZD/g,(tz < 0 ? '+' : '-') + zeroPad(Math.floor(atz / 60),2) + ':' + zeroPad(atz % 60,2));
+	t = t.replace(/\\/g,"");
+	return t;
+};
+
+util.Date.prototype.getWeek = function()
+{
+	var dt = new Date(this.getTime());
+	var d = dt.getDay();
+	if(d==0) d=7;// JavaScript Sun=0, ISO Sun=7
+	dt.setTime(dt.getTime()+(4-d)*86400000);// shift day to Thurs of same week to calculate weekNo
+	var n = Math.floor((dt.getTime()-new Date(dt.getFullYear(),0,1)+3600000)/86400000);
+	return Math.floor(n/7)+1;
+};
+
+util.Date.prototype.getYearForWeekNo = function()
+{
+	var dt = new Date(this.getTime());
+	var d = dt.getDay();
+	if(d==0) d=7;// JavaScript Sun=0, ISO Sun=7
+	dt.setTime(dt.getTime()+(4-d)*86400000);// shift day to Thurs of same week
+	return dt.getFullYear();
+};
+
+util.Date.prototype.getHours12 = function()
+{
+	var h = this.getHours();
+	return h > 12 ? h-12 : ( h > 0 ? h : 12 );
+};
+
+util.Date.prototype.getAmPm = function()
+{
+	return this.getHours() >= 12 ? dateS.pm : dateS.am;
+};
+
+util.Date.prototype.daySuffix = function()
+{
+	return dateS.daySuffixes[this.getDate()-1];
+};
+
+// Convert a date to local YYYYMMDDHHMM string format
+util.Date.prototype.convertToLocalYYYYMMDDHHMM = function()
+{
+	return this.getFullYear() + zeroPad(this.getMonth()+1,2) + zeroPad(this.getDate(),2) + zeroPad(this.getHours(),2) + zeroPad(this.getMinutes(),2);
+};
+
+// Convert a date to UTC YYYYMMDDHHMM string format
+util.Date.prototype.convertToYYYYMMDDHHMM = function()
+{
+	return this.getUTCFullYear() + zeroPad(this.getUTCMonth()+1,2) + zeroPad(this.getUTCDate(),2) + zeroPad(this.getUTCHours(),2) + zeroPad(this.getUTCMinutes(),2);
+};
+
+// Convert a date to UTC YYYYMMDD.HHMMSSMMM string format
+util.Date.prototype.convertToYYYYMMDDHHMMSSMMM = function()
+{
+	return this.getUTCFullYear() + zeroPad(this.getUTCMonth()+1,2) + zeroPad(this.getUTCDate(),2) + "." + zeroPad(this.getUTCHours(),2) + zeroPad(this.getUTCMinutes(),2) + zeroPad(this.getUTCSeconds(),2) + zeroPad(this.getUTCMilliseconds(),3) +"0";
+};
+
+// Static method to create a date from a UTC YYYYMMDDHHMM format string
+util.Date.convertFromYYYYMMDDHHMM = function(d)
+{
+	d = d?d.replace(/[^0-9]/g, ""):"";
+	return util.Date.convertFromYYYYMMDDHHMMSSMMM(d.substr(0,12));
+};
+
+// Static method to create a date from a UTC YYYYMMDDHHMMSS format string
+util.Date.convertFromYYYYMMDDHHMMSS = function(d)
+{
+	d = d?d.replace(/[^0-9]/g, ""):"";
+	return util.Date.convertFromYYYYMMDDHHMMSSMMM(d.substr(0,14));
+};
+
+// Static method to create a date from a UTC YYYYMMDDHHMMSSMMM format string
+util.Date.convertFromYYYYMMDDHHMMSSMMM = function(d)
+{
+	d = d ? d.replace(/[^0-9]/g, "") : "";
+	return new Date(Date.UTC(parseInt(d.substr(0,4),10),
+			parseInt(d.substr(4,2),10)-1,
+			parseInt(d.substr(6,2),10),
+			parseInt(d.substr(8,2)||"00",10),
+			parseInt(d.substr(10,2)||"00",10),
+			parseInt(d.substr(12,2)||"00",10),
+			parseInt(d.substr(14,3)||"000",10)));
+};
+var dateS ={};
+dateS.months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November","December"];
+dateS.days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+dateS.shortMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+dateS.shortDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// suffixes for dates, eg "1st","2nd","3rd"..."30th","31st"
+dateS.daySuffixes = ["st","nd","rd","th","th","th","th","th","th","th",
+		"th","th","th","th","th","th","th","th","th","th",
+		"st","nd","rd","th","th","th","th","th","th","th",
+		"st"];
+dateS.am = "am";
+dateS.pm = "pm";
+
 var TiddlyFox = {
 
 	// Name of the permission for TiddlyFox used by Firefox
@@ -80,6 +264,26 @@ var TiddlyFox = {
 		// Attach the event handler to the message box
 		messageBox.addEventListener("tiddlyfox-save-file",TiddlyFox.onSaveFile,false);
 	},
+ copyFile: function(sourcefile,destdir)
+{
+  // get a component for the file to copy
+  var aFile = Components.classes["@mozilla.org/file/local;1"]
+    .createInstance(Components.interfaces.nsILocalFile);
+  if (!aFile) return false;
+
+  // get a component for the directory to copy to
+  var aDir = Components.classes["@mozilla.org/file/local;1"]
+    .createInstance(Components.interfaces.nsILocalFile);
+  if (!aDir) return false;
+
+  // next, assign URLs to the file components
+  aFile.initWithPath(sourcefile);
+  aDir.initWithPath(TiddlyFox.getSpecialBackupPath(sourcefile));
+
+  // finally, copy the file
+  
+  aFile.copyTo(aDir,TiddlyFox.getSpecialBackupFile(sourcefile));
+},
 
 	saveFile: function(filePath,content) {
 		// Attempt to convert the filepath to a proper UTF-8 string
@@ -90,8 +294,10 @@ var TiddlyFox = {
 		}
 		// Save the file
 		try {
+			TiddlyFox.copyFile(filePath,"/media/3497f82e-3b95-41de-90af-df905eceeab4/data/radice/firefoxextension/Tw516/plugins/etc");
 			var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
 			file.initWithPath(filePath);
+
 			if(!file.exists())
 				file.create(0,0x01B4);// 0x01B4 = 0664
 			var out = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance(Components.interfaces.nsIFileOutputStream);
@@ -108,6 +314,35 @@ var TiddlyFox = {
 		}
 	},
 
+getSpecialBackupPath: function(localPath) {
+	var slash = "\\";
+	var dirPathPos = localPath.lastIndexOf("\\");
+	if(dirPathPos == -1) {
+		dirPathPos = localPath.lastIndexOf("/");
+		slash = "/";
+	}
+	var backupFolder = "backups";//config.options.txtBackupFolder;
+	if(!backupFolder || backupFolder == "")
+		backupFolder = ".";
+	backupFolder=localPath.substr(0,dirPathPos) + slash + backupFolder;alert(backupFolder);
+	return backupFolder;
+},
+getSpecialBackupFile: function(localPath) {
+	var slash = "\\";
+	var dirPathPos = localPath.lastIndexOf("\\");
+	if(dirPathPos == -1) {
+		dirPathPos = localPath.lastIndexOf("/");
+		slash = "/";
+	} 
+	var backupPath = localPath.substr(dirPathPos+1);
+	backupPath = backupPath.substr(0,backupPath.lastIndexOf(".")) + ".";
+        //if (config.options.chkOneADayBackUpFile==true)
+	        backupPath += (new util.Date()).convertToYYYYMMDDHHMMSSMMM().replace (/(.*)\.(.*)/,"$1") + "." + "html";
+        //else 
+	     //   backupPath += (new Date()).convertToYYYYMMDDHHMMSSMMM() + "." + "html";
+	     alert(backupPath);
+	return backupPath;
+},
 	onSaveFile: function(event) {
 		// Get the details from the message
 		var message = event.target,
@@ -180,4 +415,8 @@ window.addEventListener("load",function load(event) {
 	TiddlyFox.onLoad(event);
 },false); 
 return TiddlyFox;
+
+
+
+
 }());
